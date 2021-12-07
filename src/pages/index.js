749b02884839b80bbcb576b814096dc75cd8c1ea
polina-g <ignatieva.polina@gmail.com>
2021-12-07 14:34:54 -0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const HomePage = () => {
+  return(
+    <div>
+      <h1>Welcome!</h1>
+      <h2>My name is Polina Gorbunova</h2>
+    </div>
+  );
+};
+
+export default HomePage;
