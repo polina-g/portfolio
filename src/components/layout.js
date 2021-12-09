790a9eb8fe1ef1d89a51af2ce4ee355b6651f4cc
polina-g <ignatieva.polina@gmail.com>
2021-12-09 17:10:54 -0500

    adds contact form, custom material ui theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,16 +3,28 @@ import Header from "./header";
 import Footer from "./footer";
 import '../styles/index.scss'
 import {layoutContainer} from './layout.module.scss'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Layout = ({ children }) => {
+    const theme = createTheme({
+        palette: {
+            background: {
+                paper: 'rgba(255, 255, 255, 0.9)',
+                default: '#f6f6f6',
+            }
+        }
+      });
+
     return (
-        <div className={layoutContainer}>
-            <Header />
-                <main>
-                    {children}
-                </main>
-            <Footer />
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className={layoutContainer}>
+                <Header />
+                    <main>
+                        {children}
+                    </main>
+                <Footer />
+            </div>
+        </ThemeProvider>
     );
 };
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,83 @@
 import React from 'react';
+import { useState } from 'react';
 import Layout from '../components/layout';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/system/Box';
+import Paper from '@mui/material/Paper';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
 
 const ContactPage = () => {
+    const [formState, setFormState] = useState({
+        full_name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (event) => {
+        setFormState(prevState => ({
+          ...prevState,
+          [event.target.name]: event.target.value
+        }));
+      };
+
     return (
         <div>
             <Layout>
-            <h1>Contact Me</h1>
+                <Typography variant="h2">CONTACT ME</Typography>
+                <Box 
+                    sx={{width: '70%', mt:5, ml:'15%'}}
+                    justifyContent='center'
+                    alignItems='center'
+                >
+                    <Paper elevation={5} sx={{width: '100%', p: '1rem'}}>
+                        <FormControl>
+                            <Typography
+                                variant='h3'
+                                color='primary'
+                                fontWeight='light'
+                                sx={{mt: 3, mb: 3}}
+                            >
+                            Send me and e-mail 
+                            </Typography>
+                            <TextField
+                            type='text'
+                            label='Full Name'
+                            autoComplete='off'
+                            margin='normal'
+                            onChange={handleChange}
+                            value={formState.full_name}
+                            name='full_name'
+                            required
+                            helperText="Please enter your full name"
+                            />
+
+                            <TextField
+                            type='text'
+                            label='E-mail'
+                            name='email'
+                            autoComplete='off'
+                            margin='normal'
+                            fullWidth
+                            required
+                            onChange={handleChange}
+                            value={formState.email}
+                            sx={{mb: 3}}
+                            helperText="Please enter your email"
+                            />
+
+                            <TextField
+                            label="Message"
+                            multiline
+                            maxRows={10}
+                            value={formState.message}
+                            onChange={handleChange}
+                            />
+
+                        </FormControl>
+                    </Paper>    
+                </Box>
+
             </Layout>
         </div>
     );
