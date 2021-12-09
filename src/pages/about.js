a4c6429d29319f09837cd5acac6e8d86f499e8e7
polina-g <ignatieva.polina@gmail.com>
2021-12-09 16:34:40 -0500

    adds resume page styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { Typography } from '@mui/material';
 
 const AboutPage = () => {
     return(
         <div>
             <Layout>
-            <h1>About Page</h1>
+            <Typography variant="h2">ABOUT ME</Typography>
             <p>My name is Polina. Here is my story</p>
             <p>Find me on <a href="https://www.github.com/polina-g" target="_blank">Github!</a></p>
             </Layout>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,12 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Layout from '../components/layout';
+import Typography from '@mui/material/Typography';
+import {resumeContainer} from './resume.module.scss'
 
 const ResumePage = () => {
     return (
         <div>
             <Layout>
-                <h1>My Resume</h1>
-                <iframe src="https://drive.google.com/file/d/1NRRk0_a1eF4gDwdWrjzGwW_18l30hr6v/preview" width="800" height="480" allow="autoplay"></iframe>
+                <Typography variant="h2">RESUME</Typography>
+                <div className={resumeContainer}>
+                    <iframe src="https://drive.google.com/file/d/1NRRk0_a1eF4gDwdWrjzGwW_18l30hr6v/preview" width="900" height="480" allow="autoplay"></iframe>
+                </div>
             </Layout>
         </div>
     );
