abe99c7437197883ef0c46e01da52cdcbb569e46
polina-g <ignatieva.polina@gmail.com>
2021-12-12 17:46:21 -0500

    creates separate navbar component with full size navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,9 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { nav, activeLink, link } from './header.module.scss';
-import { StaticImage } from 'gatsby-plugin-image';
+import Navbar from './navbar'
 
 const Header = () => {
     return (
-        <header>
-            <nav className = {nav}>
-                <ul>
-                    <li>
-                        <Link 
-                            className={link}
-                            activeClassName={activeLink} to="/about">About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            className={link}
-                            activeClassName={activeLink} to="/portfolio">Portfolio</Link>
-                    </li>
-                    <li>
-                        <Link 
-                            className={link} 
-                            activeClassName={activeLink} to="/"><StaticImage alt="avatar outline" src="../../static/portfolio-avatar.png" width={60}/>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link 
-                            className={link}
-                            activeClassName={activeLink} to="/resume">Resume</Link>
-                    </li>
-                    <li>
-                        <Link 
-                            className={link}
-                            activeClassName={activeLink} to="/contact">Contact Me</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <Navbar />
     );
 };
 
