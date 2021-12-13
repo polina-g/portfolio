e25883be756ed2209649974d1a5fc8944f1ac57f
polina-g <ignatieva.polina@gmail.com>
2021-12-12 20:27:24 -0500

    adds mobile response index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import {indexContainer, contactButtons, introContainer} from './index.module.scss'
+import useMediaQuery from '../utils/useMediaQuery';
 import Typography from '@mui/material/Typography';
 import Slide from '@mui/material/Slide';
 import Fade from '@mui/material/Fade';
@@ -11,12 +12,14 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 const HomePage = () => {
+  const isQuery = useMediaQuery('(max-width: 380px)');
   const [slideOne, setSlideOne] = useState(false)
   const [slideTwo, setSlideTwo] = useState(false)
   const [slideThree, setSlideThree] = useState(false)
   const [fadeButtonOne, setFadeButtonOne] = useState(false)
   const [fadeButtonTwo, setFadeButtonTwo] = useState(false)
   const [fadeButtonThree, setFadeButtonThree] = useState(false)
+  console.log(isQuery);
 
   const handleSlide = () => {
     let timeout = 500
@@ -44,13 +47,14 @@ const HomePage = () => {
 
   useEffect(() => {
     handleSlide();
-  }, [])
+  }, [isQuery])
 
-  return(
-    <div>
+  const desktop = () => {
+    return (
+      <div>
       <Layout>
-        <div className={indexContainer}>
-          <div className={introContainer}>
+    <div className={indexContainer}>
+        <div className={introContainer}>
           <Slide direction="right" in={slideOne}>
             <Typography variant="h1" color="secondary" sx={{ml: "5%"}}>
               Hey there!
@@ -66,8 +70,8 @@ const HomePage = () => {
               I am a Software Developer.
             </Typography>
           </Slide>
-          </div>
-          <div className={contactButtons}>
+        </div>
+        <div className={contactButtons}>
             <Fade in={fadeButtonOne}>
               <Button variant="outlined" startIcon={<GitHubIcon />} size="large">
                   <a href="https://github.com/polina-g" target="_blank" rel="noreferrer">GITHUB</a>
@@ -83,10 +87,60 @@ const HomePage = () => {
                   <a href="https://twitter.com/ThisIsPolinaG" target="_blank" rel="noreferrer">TWITTER</a>
               </Button>
             </Fade>
+        </div>
+    </div>
+    </Layout>
+    </div>
+    )};
+
+  const mobile = () => {
+  return (
+    <div>
+      <Layout>
+        <div className={indexContainer}>
+          <div className={contactButtons}>
+              <Fade in={fadeButtonOne}>
+                <Button variant="outlined" size="small" startIcon={<GitHubIcon />}>
+                    <a href="https://github.com/polina-g" target="_blank" rel="noreferrer">GITHUB</a>
+                </Button>
+              </Fade>
+              <Fade in={fadeButtonTwo}>
+                <Button variant="outlined" size="small" startIcon={<LinkedInIcon />}>
+                    <a href="https://www.linkedin.com/in/polina-g/" target="_blank" rel="noreferrer">LINKEDIN</a>
+                </Button>
+              </Fade>
+              <Fade in={fadeButtonThree}>
+                <Button variant="outlined" size="small" startIcon={<TwitterIcon />}>
+                    <a href="https://twitter.com/ThisIsPolinaG" target="_blank" rel="noreferrer">TWITTER</a>
+                </Button>
+              </Fade>
+          </div>
+          <div className={introContainer}>
+            <Slide direction="right" in={slideOne}>
+              <Typography variant="h3" color="secondary" sx={{ml: "5%", mt:"20px;"}}>
+                Hey there!
+              </Typography>
+            </Slide>
+            <Slide direction="up" in={slideTwo}>
+              <Typography variant="h4" color="primary" sx={{mt:'10px'}}>
+                My name is Polina
+              </Typography>
+            </Slide>
+            <Slide direction="up" in={slideThree}>
+              <Typography variant="h5" color="primary" sx={{mt: "10px"}}>
+                I am a Software Developer.
+              </Typography>
+            </Slide>
           </div>
         </div>
       </Layout>
     </div>
+    )}
+  
+  return(
+  
+        isQuery ? mobile() : desktop()
+      
   );
 };
 
